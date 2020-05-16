5952ed634a7a5e66b28306e18b6313df455ee060
Stoian-Mihai <mihaistoianro@gmail.com>
2020-05-16 15:30:52 +0300

    Create gamelist.jsx

---
+++ frontend/bowtie/src/pages/gamelist.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const gamelist = () => (
+  <div>
+    <h1>Hi people</h1>
+  </div>
+)
+
+export default gamelist
