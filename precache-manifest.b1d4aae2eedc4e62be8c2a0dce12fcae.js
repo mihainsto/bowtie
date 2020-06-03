9de7ec5a81db0a1dc406aef20c4b24b57172a202
mihainsto <mihaistoianro@gmail.com>
2020-06-03 23:30:45 +0300

    Updates

---
+++ precache-manifest.b1d4aae2eedc4e62be8c2a0dce12fcae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab098f0ca2a2ca9af6d2bf8aadd2408",
+    "url": "/bowtie/index.html"
+  },
+  {
+    "revision": "fcc7234724c75a5cd663",
+    "url": "/bowtie/static/css/main.872de18d.chunk.css"
+  },
+  {
+    "revision": "953e5faa570844b27024",
+    "url": "/bowtie/static/js/2.e024bd1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bowtie/static/js/2.e024bd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc7234724c75a5cd663",
+    "url": "/bowtie/static/js/main.726dad96.chunk.js"
+  },
+  {
+    "revision": "7ca6a39a146ba903edd4",
+    "url": "/bowtie/static/js/runtime-main.9b1f78fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bowtie/static/media/logo.5d5d9eef.svg"
+  }
+]);
