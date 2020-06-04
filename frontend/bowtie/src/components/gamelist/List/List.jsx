16f81a7ebc31fe397656063b022ac2790339fb90
mihainsto <mihaistoianro@gmail.com>
2020-06-05 00:03:42 +0300

    list vertical scroll desktop devices

--- frontend/bowtie/src/components/gamelist/List/List.jsx
+++ frontend/bowtie/src/components/gamelist/List/List.jsx
@@ -5,64 +5,69 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import { useState, useRef } from "react"
+import React from "react";
+import { useState, useRef } from "react";
 
 //import TitleCard from "/../TitleCard/TitleCard"
-import TitleCard from "../TitleCard/TitleCard"
-import Card from "../Card/Card"
-import AddNewCard from "../AddNewCard/AddNewCard"
-import SearchGameCard from "../SearchGameCard/SearchGameCard"
-import "./List.scss"
+import TitleCard from "../TitleCard/TitleCard";
+import Card from "../Card/Card";
+import AddNewCard from "../AddNewCard/AddNewCard";
+import SearchGameCard from "../SearchGameCard/SearchGameCard";
+import "./List.scss";
 
-const List = props => {
-  const [newEntry, setNewEntry] = useState("")
-  const [addButtonVisibile, setAddButtonVisibile] = useState("list-visibility-visible")
-  const [searchVisible, setSearchVisible] = useState("list-visibility-hidden")
-  const searchElement = useRef(null)
+const List = (props) => {
+  const [newEntry, setNewEntry] = useState("");
+  const [addButtonVisibile, setAddButtonVisibile] = useState(
+    "list-visibility-visible"
+  );
+  const [searchVisible, setSearchVisible] = useState("list-visibility-hidden");
+  const searchElement = useRef(null);
 
-  const searchOnChangeValueHandler = val => {
-    setNewEntry(val.target.value)
-  }
+  const searchOnChangeValueHandler = (val) => {
+    setNewEntry(val.target.value);
+  };
 
   const addNewClicked = () => {
-    searchElement.current.focus()
-  }
-  const searchFocused = () =>{
-    setAddButtonVisibile("list-visibility-hidden")
-    setSearchVisible("list-visibility-visible")
-  }
-  const searchBlured = () =>{
-    setAddButtonVisibile("list-visibility-visible")
-    setSearchVisible("list-visibility-hidden")
-  }
+    searchElement.current.focus();
+  };
+  const searchFocused = () => {
+    setAddButtonVisibile("list-visibility-hidden");
+    setSearchVisible("list-visibility-visible");
+  };
+  const searchBlured = () => {
+    setAddButtonVisibile("list-visibility-visible");
+    setSearchVisible("list-visibility-hidden");
+  };
   return (
-    <div className="list-list">
+    <div className="list-list-wrapper">
       <div className="list-title-card">
         <TitleCard title={props.listItems.title} />
       </div>
-
-      {props.listItems.items.map(item => (
-        <div className="list-card">
-          <Card cardText={item} />
+      <div className="list-list">
+        {props.listItems.items.map((item) => (
+          <div className="list-card">
+            <Card cardText={item} />
+          </div>
+        ))}
+        <div className="list-card" className={searchVisible}>
+          <SearchGameCard
+            onChangeValue={searchOnChangeValueHandler}
+            ref={searchElement}
+            focused={searchFocused}
+            blured={searchBlured}
+          />
         </div>
-      ))}
-      <div className="list-card" className = {searchVisible}>
-        <SearchGameCard onChangeValue={searchOnChangeValueHandler} 
-        ref = {searchElement} focused = {searchFocused} blured = {searchBlured}
-        />
-      </div>
 
-      <div
-        className="list-card"
-        className = {addButtonVisibile}
-        onClick={addNewClicked}
-      >
-        <AddNewCard cardText="+ Add new game" />
+        <div
+          className="list-card"
+          className={addButtonVisibile}
+          onClick={addNewClicked}
+        >
+          <AddNewCard cardText="+ Add new game" />
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
