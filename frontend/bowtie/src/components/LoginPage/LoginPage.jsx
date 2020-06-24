7674d3ae0b72de706bbeb99838d03f365fa51057
mihainsto <mihaistoianro@gmail.com>
2020-06-24 12:07:44 +0300

    added input text fields on login page

--- frontend/bowtie/src/components/LoginPage/LoginPage.jsx
+++ frontend/bowtie/src/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./LoginPage.scss";
-import LogoNav from "../Navs/LogoNav/LogoNav"
+import LogoNav from "../Navs/LogoNav/LogoNav";
 const LoginPage = () => {
   return (
     <div className="loginpage">
       <LogoNav />
-      <div>Login Page</div>
+      <div className="form-container">
+        <div className="buttons">
+          <span className="login-btn">Log In</span>
+          <span className="register-btn">Register</span>
+        </div>
+
+        <div>
+          <input type="email" placeholder="Email"></input>
+        </div>
+        <div>
+          <input type="password" placeholder="Password"></input>
+        </div>
+        </div>
     </div>
   );
 };
