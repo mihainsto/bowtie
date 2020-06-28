f9afd8907f205e99db5205141689ff14dcdc68fd
mihainsto <mihaistoianro@gmail.com>
2020-06-28 11:34:35 +0300

    add request for register

---
+++ backend/database/database.js
@@ -0,0 +1,8 @@
+const add_user = (email, username, password) => {
+    console.log({email: email, username: username, password: password})
+}
+
+
+module.exports = {
+    add_user,
+  };
--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const DB = require('./database/database') 
 const app = express()
 const port = 3000
 
@@ -12,9 +14,23 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/auth', (req, res) =>{
 
-    request = req.body
-    
     res.send('auth')
 })
 
+app.post('/register', async (req, res) => {
+    try {
+        console.log(req.body)
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        console.log(salt)
+        console.log(hashedPassword)
+        DB.add_user(req.body.email, req.body.username, hashedPassword)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send()
+    }
+    finally{
+        res.send({status: 'success'})
+    }
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
