886e0719f4ac82d64aee021111985363338c45cf
mihainsto <mihaistoianro@gmail.com>
2020-06-28 11:14:36 +0300

    added bodyparser to fix post request

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/auth', (req, res) =>{
 
-    return("AUTH")
+    request = req.body
+    
+    res.send('auth')
 })
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
