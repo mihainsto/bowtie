ecad1333947a3cf8f6df9aa4d21947d5bf20b8b7
mihainsto <mihaistoianro@gmail.com>
2020-07-05 11:41:22 +0300

    added prop value height to addNewCard component

--- frontend/bowtie/src/components/gamelist/AddNewCard/AddNewCard.jsx
+++ frontend/bowtie/src/components/gamelist/AddNewCard/AddNewCard.jsx
@@ -3,8 +3,15 @@ import React from "react"
 import "./AddNewCard.scss"
 
 const AddNewCard = (props) => {
+
+  const divStyle = {
+    height: props.height.toString() + 'px',
+    lineHeight: props.height.toString() + 'px'
+  };
+
+  console.log(divStyle)
     return (
-      <div className="addnewcard-card">
+      <div className="addnewcard-card" style={divStyle}>
         <div className="addnewcard-text">
           {props.cardText}
         </div>
