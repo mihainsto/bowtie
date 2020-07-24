4623faa4e6f9d3a27f9eb62f910730e6fb6a4165
mihainsto <mihaistoianro@gmail.com>
2020-07-24 15:54:52 +0300

    changed image rendering to hide the image while is loading

--- frontend/bowtie/src/components/gamelist/CardImage/CardImage.jsx
+++ frontend/bowtie/src/components/gamelist/CardImage/CardImage.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "./CardImage.scss"
+import "./CardImage.scss";
 const CardImage = (props) => {
-  const [displayImage, setDisplayImage] = useState("true")
+  const [displayImage, setDisplayImage] = useState(false);
   const cropSize = { x: 0, y: 0, height: 200, width: 200 };
   const withHttp = (url) =>
     !/^https?:\/\//i.test(url) ? `https://${url}` : url;
-  
-  useEffect(() => {
-    setDisplayImage(true)
-  }, [props.image])
-  
-  const imageDoseNotExist = () => {
-    setDisplayImage(false)
-  }
 
-  if (displayImage){
+  useEffect(() => {
+    setDisplayImage(true);
+  }, [props.image]);
+
+  const imageDoseNotExist = () => {
+    setDisplayImage(false);
+  };
+  const imageLoaded = () => {
+    setDisplayImage(true);
+  };
     return (
-      <div className="cardImage">
-        <img src={withHttp(props.image)} onError={imageDoseNotExist}></img>
+      <div className={displayImage?"":"display-none"}>
+        <div className="cardImage">
+          <img
+            src={withHttp(props.image)} 
+            onError={imageDoseNotExist}
+            onLoad={imageLoaded}
+            alt=""
+          ></img>
+        </div>
       </div>
     );
-  } else{
-    return <div></div>
   }
-  
 
-};
 
 export default CardImage;
