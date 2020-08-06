f4d928272c2cdafff45aecbbe66aae02518a879e
mihainsto <mihaistoianro@gmail.com>
2020-08-06 19:58:58 +0300

    added blank menu that opens on menu button press

--- frontend/bowtie/src/components/Navs/MainNav/MainNav.jsx
+++ frontend/bowtie/src/components/Navs/MainNav/MainNav.jsx
@@ -3,25 +3,43 @@ import "./MainNav.scss";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "components/Navs/Menu/Menu";
+import {useState} from "react";
 import MenuOpenIcon from "@material-ui/icons/MenuOpen";
 const MainNav = () => {
-  return (
-    <nav className="main-nav">
-      <span className="logo">Bowtie</span>
-      <span className="buttons-container">
-        <span className="username-button">
-          Username
-          <ExpandMoreIcon className="icon" />
-        </span>
-        <span>
-          <NotificationsNoneIcon className="icon" />
-        </span>
+  const [menuToggle, setMenuToggle] = useState(false);
 
-        <span>
-          <MenuIcon className="icon" />
+  const toggleMenu = () => {
+    if (menuToggle === true){
+      setMenuToggle(false)
+    } else
+    {
+      setMenuToggle(true)
+    }
+  }
+  return (
+    <div>
+      <nav className="main-nav">
+        <span className="logo">Bowtie</span>
+        <span className="buttons-container">
+          <span className="username-button">
+            Username
+            <ExpandMoreIcon className="icon" />
+          </span>
+          <span>
+            <NotificationsNoneIcon className="icon" />
+          </span>
+
+          <span onClick={toggleMenu}>
+            {!menuToggle? <MenuIcon className="icon"/>:<MenuOpenIcon className="icon" />}
+          </span>
         </span>
-      </span>
-    </nav>
+        
+      </nav>
+        <div className={!menuToggle? "display-none": ""} >
+        <Menu />
+        </div>
+    </div>
   );
 };
 
