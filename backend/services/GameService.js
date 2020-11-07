2ed69102c91735b4df0fb4e3c2af871900bc18b3
mihainsto <44275480+mihainsto@users.noreply.github.com>
2020-11-07 13:23:46 +0200

    Upgrade backend to igdb v4 (#94)
    * Upgrade backend to igdb v4
    
    * Update readme for igdb v4
    
    * Update README.md
    
    * Update README.md

--- backend/services/GameService.js
+++ backend/services/GameService.js
@@ -4,47 +4,48 @@ const smartcrop = require("./smartcrop/smartcrop");
 const paths = require("../config/paths");
 
 const get_games = async (user) => {
-  try {
-    gameIds = [];
-    user.board.cards.forEach((card) => {
-      gameIds.push(card.gameId);
-    });
-    const games = await Game.find({ gameId: { $in: gameIds } });
-    const gamesObj = games.reduce((a, x) => ({ ...a, [x.gameId]: x }), {});
-    return gamesObj;
-  } catch (err) {
-    throw err;
-  }
+    try {
+        gameIds = [];
+        user.board.cards.forEach((card) => {
+            gameIds.push(card.gameId);
+        });
+        const games = await Game.find({gameId: {$in: gameIds}});
+        const gamesObj = games.reduce((a, x) => ({...a, [x.gameId]: x}), {});
+        return gamesObj;
+    } catch (err) {
+        throw err;
+    }
 };
 
 // Creates a game or returns one if it exists
 const create_game = async (gameId) => {
-  const games = await Game.find({ gameId: gameId });
-  if (games.length) {
-      return games[0]
-  } else {
-    // searching igdb for game and picture
-    igdbGame = await igdb.get_game(gameId);
-    //paths.imagesPath
-    const src = igdbGame.image;
-    const newImageName = src.split("/")[src.split("/").length - 1];
-    const newImagePath = paths.imagesPath + "/games/" + newImageName;
-    const status = await smartcrop.applySmartCropPromise(
-      igdbGame.image,
-      newImagePath,
-      600,
-      300
-    );
-    const newGame = new Game({
-      gameId: gameId,
-      title: igdbGame.name,
-      imageUrl: newImagePath,
-      releaseDate: igdbGame.first_release_date,
-    });
+    const games = await Game.find({gameId: gameId});
+    if (games.length) {
+        return games[0]
+    } else {
+        // searching igdb for game and picture
+        const igdbGame = await igdb.get_game(gameId);
+        //paths.imagesPath
+        console.log(igdbGame)
+        const src = igdbGame.image;
+        const newImageName = src.split("/")[src.split("/").length - 1];
+        const newImagePath = paths.imagesPath + "/games/" + newImageName;
+        const status = await smartcrop.applySmartCropPromise(
+            igdbGame.image,
+            newImagePath,
+            600,
+            300
+        );
+        const newGame = new Game({
+            gameId: gameId,
+            title: igdbGame.name,
+            imageUrl: newImagePath,
+            releaseDate: igdbGame.first_release_date,
+        });
 
-    const game = await newGame.save();
-    return newGame;
-  }
+        const game = await newGame.save();
+        return newGame;
+    }
 };
 
 const search_game = async (query, page) => {
@@ -53,8 +54,8 @@ const search_game = async (query, page) => {
     if (query.length > 100) {
         throw "Query to big"
     }
-    const search_results = await igdb.search_for_a_game(query, (page-1)*4, 4)
+    const search_results = await igdb.search_for_a_game(query, (page - 1) * 4, 4)
     return search_results
 }
 
-module.exports = { get_games, create_game, search_game };
+module.exports = {get_games, create_game, search_game};
